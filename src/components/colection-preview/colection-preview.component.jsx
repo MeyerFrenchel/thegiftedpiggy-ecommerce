c8ec66c8dca0410b4885d859b8803bc0afc32be5
MeyerFrenchel <meyer.frenchel@gmail.com>
2020-11-23 01:59:03 +0200

    cart item reducer

--- src/components/colection-preview/colection-preview.component.jsx
+++ src/components/colection-preview/colection-preview.component.jsx
@@ -10,9 +10,9 @@ const CollectionPreview = ({title, items}) => (
         <div className='preview'>
             {
                 items
-                .filter((item, idx) => idx < 3)
-                .map(({id, ...otherItemProps}) =>(
-                    <CollectionItem key={id} {...otherItemProps}/> 
+                .filter((item, idx) => idx < 4)
+                .map((item) =>(
+                    <CollectionItem key={item.id} item={item}/> 
                 ))}
         </div>
     </div>
