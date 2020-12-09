f493bd76c3c60c0b5354dd17d380339d8a222c05
MeyerFrenchel <meyer.frenchel@gmail.com>
2020-12-09 17:40:28 +0200

    made homepage responsive

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -5,6 +5,8 @@ import Directory from '../../components/directory/directory.component';
 import './homepage.styles.scss';
 
 
+
+
 const HomePage = () => (
     <div className='homepage'>
         <Directory />
